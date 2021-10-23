786c828c66bc922bc102894ab8bdb2ffbf0419cc
cyn <1223174891@qq.com>
2021-10-23 23:38:04 +0800

    [FIX] fix replace filename not work in test pipeline

--- mmdet/datasets/coco_with_sub_image.py
+++ mmdet/datasets/coco_with_sub_image.py
@@ -8,10 +8,16 @@ class CocoDatasetWithSubImage(CocoDataset):
         self.sub_images = sub_images
         super(CocoDatasetWithSubImage, self).__init__(**kwargs)
 
+    def load_annotations(self, ann_file):
+        data_infos = super().load_annotations(ann_file)
+        for index,info in enumerate(data_infos):
+            data_infos[index]['filename'] = [os.path.join(info['filename'], sub_image_name) for sub_image_name in self.sub_images]
+        return data_infos
+
+
     def _parse_ann_info(self, img_info, ann_info):
-        ann = super()._parse_ann_info(img_info,ann_info)
-        filename_list = [os.path.join(img_info['filename'], sub_image_name) for sub_image_name in self.sub_images]
+        filename_list = img_info['filename']
+        img_info['filename'] = filename_list[0]
+        ann = super()._parse_ann_info(img_info, ann_info)
         img_info['filename'] = filename_list
-        seg_map = [x.replace('jpg', 'png') for x in filename_list]
-        ann['seg_map']=seg_map
         return ann
