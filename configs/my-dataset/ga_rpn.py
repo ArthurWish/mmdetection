13fca485d3935248ad7d4f2b78e27197436545c0
Yunnong <1223174891@qq.com>
2022-04-28 16:59:49 +0800

    add some baseline configs and some visulization utils

---
+++ configs/my-dataset/ga_rpn.py
@@ -0,0 +1,93 @@
+_base_ = ['../guided_anchoring/ga_faster_r50_fpn_1x_coco.py']
+model = dict(
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=5),
+    rpn_head=dict(
+        _delete_=True,
+        type='GARPNHead',
+        in_channels=256,
+        feat_channels=256,
+        approx_anchor_generator=dict(
+            type='AnchorGenerator',
+            octave_base_scale=8,
+            scales_per_octave=3,
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]),
+        square_anchor_generator=dict(
+            type='AnchorGenerator',
+            ratios=[1.0],
+            scales=[8],
+            strides=[4, 8, 16, 32, 64]),
+        anchor_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[0.07, 0.07, 0.14, 0.14]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[0.07, 0.07, 0.11, 0.11]),
+        loc_filter_thr=0.01,
+        loss_loc=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_shape=dict(type='BoundedIoULoss', beta=0.2, loss_weight=1.0),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='SmoothL1Loss', beta=1.0, loss_weight=1.0)),
+    roi_head=dict(
+        bbox_head=dict(
+            num_classes=1,
+            bbox_coder=dict(target_stds=[0.05, 0.05, 0.1, 0.1]))),
+    # model training and testing settings
+    train_cfg=dict(
+        rpn=dict(
+            ga_assigner=dict(
+                type='ApproxMaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                ignore_iof_thr=-1),
+            ga_sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=-1,
+            center_ratio=0.2,
+            ignore_ratio=0.5),
+        rpn_proposal=dict(nms_post=1000, max_per_img=300),
+        rcnn=dict(
+            assigner=dict(pos_iou_thr=0.6, neg_iou_thr=0.6, min_pos_iou=0.6),
+            sampler=dict(type='RandomSampler', num=256))),
+    test_cfg=dict(
+        rpn=dict(nms_post=1000, max_per_img=300), rcnn=dict(score_thr=1e-3)))
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
+
+classes = ('merge',)
+data = dict(
+    samples_per_gpu=4,  # Batch size of a single GPU
+    workers_per_gpu=4,  # Worker to pre-fetch data for each single GPU
+    train=dict(
+        img_prefix='my-dataset/train',
+        classes=classes,
+        ann_file='my-dataset/train/train.json',
+    ),
+    val=dict(
+        img_prefix='my-dataset/test',
+        classes=classes,
+        ann_file='my-dataset/test/test.json',
+    ),
+    test=dict(
+        img_prefix='my-dataset/test',
+        classes=classes,
+        ann_file='my-dataset/test/test.json',
+    )
+)
